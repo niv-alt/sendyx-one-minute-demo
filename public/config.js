c3dc4ec46a2ac319c90c21c2a10394ce685da72b
niv-alt <niv@sendy.ai>
2026-09-23 10:04:26 +0900

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,5 @@
+// Google Apps Script 웹 앱 배포 후 받은 URL을 따옴표 안에 입력하세요.
+// 예: appsScriptUrl: "https://script.google.com/macros/s/...../exec"
+window.SENDYX_CONFIG = {
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxvo3P5x691u-hesCrxgfVCBmd4xiix_ccK998gocF62IYVTvPUfPk67cAnoC54MnjMHA/exec"
+};
